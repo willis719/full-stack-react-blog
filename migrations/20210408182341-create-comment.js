78b8f564849c8fbedd1c97a204e57b8d2e82a5c4
Willie Rose <willierose87@gmail.com>
2021-04-08 14:45:04 -0500

    Add register route

---
+++ migrations/20210408182341-create-comment.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      PostId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Posts',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
+const bcrypt = require('bcrypt')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// async makes everything run in order
+router.post('/register', async (req, res) => {
+  // check for username and password on request
+  if (!req.body.username || !req.body.password) {
+    // add a return so that it stops if condition is met
+    return res.status(400).json({               
+      error: 'Please include username and password'
+    })
+  }
+
+  // check database for existing user
+  // Nothing after this runs until this is finished
+  const user = await models.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+    
+  // if exists, send error
+  if (user) {
+    return res.status(400).json({
+      error: 'Username already in use'
+    })
+  }
+
+  
+  // hash password
+  const hash = await bcrypt.hash(req.body.password, 10)
+
+  // create user
+  const newUser = await models.User.create({
+    username: req.body.username,
+    password: hash
+  })
+
+  // respond with success message
+  return res.status(201).json(newUser)
+
+})  
 
 module.exports = router;
