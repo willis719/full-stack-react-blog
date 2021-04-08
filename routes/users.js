d7d85d2fe0b86f9c6907728f04abda2dcba36447
Willie Rose <willierose87@gmail.com>
2021-04-08 15:51:55 -0500

    update session and login/logout routes

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Post localhost:3000/api/v1/users/register
 // async makes everything run in order
 router.post('/register', async (req, res) => {
   // check for username and password on request
@@ -46,6 +47,61 @@ router.post('/register', async (req, res) => {
   // respond with success message
   return res.status(201).json(newUser)
 
-})  
+});
+
+
+router.post('/login', async (req, res) => {
+  // check for username and password
+  // If not there send error
+  if (!req.body.username || !req.body.password) {
+    // add a return so that it stops if condition is met
+    return res.status(400).json({               
+      error: 'Please include username and password'
+    })
+  }
+
+  // find user from username
+  const user = await models.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+
+  // if no user, send error
+  if (!user) {
+    return res.status(404).json({
+      error: 'No user with that username found'
+    })
+  }
+
+
+  // check password
+  // checks that req.body.password matches the user password
+  const match = await bcrypt.compare(req.body.password, user.password)
+  // if no match, send error
+  if (!match) {
+    return res.status(401).json({
+      error: 'Password incorrect'
+    })
+  }
+
+  // store user info in session
+  req.session.user = user;
+
+  // respond with user info
+  res.json(user)
+});
+
+router.get('/logout', (req, res) => {
+  // clear user data from session
+  req.session.user = null;
+
+  // send success response
+  res.json({
+    success: 'Logged out successfully'
+  });
+});
+
+
 
 module.exports = router;
