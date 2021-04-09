a35bc08173b633acc39a1acbd46d4eefa967c6cb
Willie Rose <willierose87@gmail.com>
2021-04-09 14:15:21 -0500

    Added react

--- app.js
+++ app.js
@@ -27,9 +27,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter); //Makes versions of the api
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
 module.exports = app;
